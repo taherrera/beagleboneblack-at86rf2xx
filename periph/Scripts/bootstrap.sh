d189dd6de669718f1701229e9ad40fb35436ad5a
therrerac <taherrera@uc.cl>
2018-09-27 20:40:19 -0300

    sh

--- periph/Scripts/bootstrap.sh
+++ periph/Scripts/bootstrap.sh
@@ -4,19 +4,19 @@ BASEDIR=$(dirname $0)
 
 # Compile .dtbo for configuring pins
 
-dtc -O dtb -o ${BASEDIR}/BB-SPI1-01-00A0.dtbo -b 0 -@ ${BASEDIR}/BB-SPI1-01-00A0.dts 
+dtc -O dtb -o ${BASEDIR}/BB-SPI0-01-00A0.dtbo -b 0 -@ ${BASEDIR}/BB-SPI0-01-00A0.dts 
 
-cp ${BASEDIR}/BB-SPI1-01-00A0.dtbo /lib/firmware/
+cp ${BASEDIR}/BB-SPI0-01-00A0.dtbo /lib/firmware/
 
 #enable device tree overlay
 
-echo BB-SPI1-01 > /sys/devices/bone_capemgr.*/slots
+echo BB-SPI0-01 > /sys/devices/bone_capemgr.*/slots
 
 # Enable SPI0 on uEnv.txt
 
 echo optargs=quiet drm.debug=7 capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN >> /boot/uEnv.txt
 
-echo capemgr.enable_partno=BB-SPI1-01 >> /boot/uEnv.txt
+echo capemgr.enable_partno=BB-SPI0-01 >> /boot/uEnv.txt
 
 # Reset the BBB
 
