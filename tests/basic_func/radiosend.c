44d4f051f8fd9fe76ad72ca7c275bf26992495fa
therrera <taherrera@uc.cl>
2018-11-07 17:15:41 +0000

    segmentation fault

--- tests/basic_func/radiosend.c
+++ tests/basic_func/radiosend.c
@@ -9,15 +9,15 @@
 #define CSGPIO 17 /* Not movable */
 
 int main() {
-	//spi_init(SPI_BUS);
-        //spi_acquire(SPI_BUS, SPI_CS, CLOCKMODE, SPI_FREQ);
+	spi_init(SPI_BUS);
+        spi_acquire(SPI_BUS, SPI_CS, CLOCKMODE, SPI_FREQ);
 	//reg_read(AT86RF2XX_REG__TRX_STATUS) & AT86RF2XX_TRX_STATUS_MASK__TRX_STATUS;
 	//assert_awake();
 	/* Before starting, initialize */
-	init(CSGPIO, INTGPIO, SLEEPGPIO, RESETGPIO);
+	//init(CSGPIO, INTGPIO, SLEEPGPIO, RESETGPIO);
 	//spi_acquire(SPI_BUS, SPI_CS, CLOCKMODE, SPI_FREQ);
 
-	//reg_read(AT86RF2XX_REG__PHY_TX_PWR); /* Segmentation Fault */
+	reg_read(AT86RF2XX_REG__PHY_TX_PWR); /* Segmentation Fault */
 	//printf("TX_PWR: %d\n",get_txpower());
 	//printf("Channel: %d\n", get_chan());
 	//printf("Long Addr: %d, Short Addr: %d\n", get_addr_long(), get_addr_short());
