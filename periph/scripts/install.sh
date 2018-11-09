639ad14b22c363e8f33334b05fb1c5b1cbfcc180
therrerac <taherrera@uc.cl>
2018-11-09 16:53:53 -0300

    scripts

--- periph/scripts/install.sh
+++ periph/scripts/install.sh
@@ -17,9 +17,6 @@ dtc -O dtb -o ${BASEDIR}/BB-SPI0-01-00A0.dtbo -b 0 -@ ${BASEDIR}/BB-SPI0-01-00A0
 
 cp ${BASEDIR}/BB-SPI0-01-00A0.dtbo /lib/firmware/
 
-#enable device tree overlay
-
-echo BB-SPI0-01 > /sys/devices/bone_capemgr.*/slots
 
 # Enable SPI0 on uEnv.txt
 echo \#\#Enable SPI0 >> /boot/uEnv.txt
@@ -27,6 +24,13 @@ echo optargs=quiet drm.debug=7 >> /boot/uEnv.txt
 echo capemgr.enable_partno=BB-SPI0-01 >> /boot/uEnv.txt
 
 # Reset the BBB ?
+
+
+#enable device tree overlay
+
+echo BB-SPI0-01 > /sys/devices/bone_capemgr.*/slots
+ls -lh /dev/spi*
+
 
 # Absolute path to this script.
 SCRIPT=$(readlink -f "$0")
